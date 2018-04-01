b1474f5ff3766a37e68078392aa4c8843b6090b8
Sergey Gavrilov <SOGavrilov@alfabank.ru>
2018-04-01 14:41:54 +0300

    add header and content blocks to app component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,99 @@
 <template>
   <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <router-view/>
+    <div class="header">
+      <router-link :to="logoLink">
+        <img class="logo" src="./assets/logo.png">
+      </router-link>
+      <ul>
+        <li v-for="link in links" :key="link.id">
+          <router-link :to="link.href">
+            {{ link.text }}
+          </router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="content">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      logoLink: './',
+      links: [
+        {
+          id: 0,
+          href: './all_heroes',
+          text: 'All Heroes',
+        },
+        {
+          id: 1,
+          href: './favourite_heroes',
+          text: 'Favourite Heroes',
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style>
+html,
+body {
+  position: relative;
+  height: 100%;
+  width: 100%;
+  padding: 0px;
+  margin: 0px;
+}
 #app {
+  height: 100%;
+  min-width: 540px;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.header {
+  width: 100%;
+  height: 60px;
+  padding: 20px 28px;
+  text-align: left;
+}
+
+.content {
+  height: calc(100% - 60px);
 }
 
 .logo {
-  width: 100%;
-  max-width: 600px;
+  height: 100%;
+}
+
+ul {
+  display: inline-block;
+  vertical-align: top;
+  list-style-type: none;
+  padding: 16px;
+  margin: 0px;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  font-size: 1.4em;
+  font-weight: bold;
+  color: #000;
+  transition: color 0.3s;
+}
+
+a:hover {
+  color: #f0141e;
 }
 </style>
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -3,13 +3,6 @@
     <h1>{{ title }}</h1>
     <h2>{{ description }}</h2>
     <h2>{{ appeal }}</h2>
-    <ul>
-      <li v-for="link in links" :key="link.id">
-        <router-link :to="link.href">
-          {{ link.text }}
-        </router-link>
-      </li>
-    </ul>
   </div>
 </template>
 
@@ -21,18 +14,6 @@ export default {
       title: 'Welcome to my Vue.js App',
       description: 'This App allows you to see at the all marvel heroes and choose your favourite',
       appeal: 'Let\'s try it!',
-      links: [
-        {
-          id: 0,
-          href: './all_heroes',
-          text: 'All Heroes',
-        },
-        {
-          id: 1,
-          href: './favourite_heroes',
-          text: 'Favourite Heroes',
-        },
-      ],
     };
   },
 };
@@ -43,22 +24,8 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  font-size: 1.4em;
-  font-weight: bold;
-  color: #000;
-  transition: color 0.3s;
-}
 
-a:hover {
-  color: #f0141e;
+.welcome {
+  margin: 100px auto;
 }
 </style>
